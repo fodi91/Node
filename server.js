4e28b282b9de997374a18388ef654a826f389978
fodi91 <fodi91@gmail.com>
2022-05-22 21:37:21 +0200

    'two'

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
 app.get("/", (reg, res) => {
   console.log("I am here");
-  res.json({ message: "error" });
-  res.download("server.js");
+  //res.json({ message: "error" });
+  //res.download("server.js");
+  res.render("index", { text: "world" });
+});
+
+app.get("/users", (req, res) => {
+  res.send("user list");
+});
+app.get("/users/new", (req, res) => {
+  res.send("user form");
 });
 app.listen(process.env.PORT || 3000);
